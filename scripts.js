3817c791159acbfd92061ab2d458d1e02a2ef44a
assessdocs <sistemaautodocs+githubnova@gmail.com>
2026-02-26 14:10:21 -0300

    Atualização

--- scripts.js
+++ scripts.js
@@ -254,8 +254,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function() {
     var perfis = {
-        perfil1: { nome: "Seven Assessoria", imagem: "perfil1.png" },
-        perfil2: { nome: "Banco Master", imagem: "perfil2.png" },
+        perfil1: { nome: "Garra Assessoria", imagem: "perfil1.png" },
+        perfil2: { nome: "Banco Agibank", imagem: "perfil2.png" },
     };
 
     var perfilAtual = 1;
